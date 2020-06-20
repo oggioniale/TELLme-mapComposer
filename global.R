9e0a9498a74cee047a3c83aa6adc3d685a9aefa5
Alessandro Oggioni <oggioniale@gmail.com>
2020-06-20 10:54:39 +0200

    base data and selectize are done

--- global.R
+++ global.R
@@ -1,15 +1,14 @@
 library(dplyr)
 # library(WikidataQueryServiceR)
-library(leaflet.extras)
 
 ###
 # metropolis
 ###
+### Cities points
 # probably this website could be a good started point https://www.metropolis.org/members-list
 # the list of metropolis is taken from wikidata
 # WikiData query for obtain the list of the cities with millions of inhabitants (https://www.wikidata.org/wiki/Q1637706)
 tableMetropolisWikiData <- WikidataQueryServiceR::query_wikidata(
-  # add population
   'SELECT DISTINCT ?city ?cityLabel ?gps ?OSM_relation_ID ?population WHERE {
   #?city wdt:P31 wd:Q1637706;
   ?city wdt:P31 wd:Q515;
@@ -20,45 +19,45 @@ tableMetropolisWikiData <- WikidataQueryServiceR::query_wikidata(
 }'
 )
 
-osmdata::set_overpass_url("https://overpass-api.de/api/interpreter")
-
-  # queryCityBoundary <- '
-  # [out:json][timeout:25];
-  # (
-  #   relation(1224652);
-  # );
-  # out body;
-  # >;
-  # out skel qt;'
-  # 
-  # cityBoundary <- osmdata_sf(queryCityBoundary)
-  # names(cityBoundary$osm_multipolygons$geometry) <- NULL
-  # cityBoundary$osm_multipolygons %>%
-  #   head(1) %>%
-  #   leaflet %>%
-  #   addTiles %>%
-  #   addPolygons()
-  
-
-  
-cityBoundaryResult <- osmdata::opq_osm_id(id = 1224652, type = 'relation', open_url = FALSE) %>%
-  osmdata::opq_string() %>%
-  osmdata::osmdata_sf()
-
-names(cityBoundaryResult$osm_multipolygons$geometry[[1]][[1]]) <- NULL
-
-tableMetropolis <- tableMetropolisWikiData %>% filter(cityLabel %in% c('Buenos Aires', 'Guadalajara', 'Seville', 'Milan'))
-tableMetropolis <- tableMetropolis %>% 
+# TODO add views parameters, geometry
+dt_Metropolis <- tableMetropolisWikiData %>% filter(cityLabel %in% c('Buenos Aires', 'Guadalajara', 'Seville', 'Milan'))
+dt_Metropolis <- dt_Metropolis %>% 
   add_row(city = '', cityLabel = 'All metropolis', gps = 'Point(0, 0)', OSM_relation_ID = 0, .before = 1) %>% 
-  add_row(city = 'https://www.wikidata.org/wiki/Q1010', cityLabel = 'Maribor', gps = 'Point(46.55472 15.64667)', OSM_relation_ID = 8154489) %>% 
-  add_row(city = 'https://www.wikidata.org/wiki/Q1489', cityLabel = 'Mexico City', gps = 'Point(19.42847 -99.12766)', OSM_relation_ID = 1376330)
+  add_row(city = 'https://www.wikidata.org/wiki/Q1010', cityLabel = 'Maribor', gps = 'Point(15.64667 46.55472)', OSM_relation_ID = 8154489) %>%
+  add_row(city = 'https://www.wikidata.org/wiki/Q1489', cityLabel = 'Mexico City', gps = 'Point(-99.12766 19.42847)', OSM_relation_ID = 1376330)
+citiesGPS <- sf::st_as_sf(dt_Metropolis, wkt = 'gps')
+sf::st_crs(citiesGPS) = 4326
 
-leaflet::leaflet() %>% 
-  leaflet::addTiles() %>% 
-  leaflet::addPolygons(
-    data = cityBoundaryResult$osm_multipolygons, 
-    group = 'metropolis',
-    weight = 2,
-    col = 'red', 
-    fillColor = 'red')
+### City multipolygons
+# osmdata::set_overpass_url("https://overpass-api.de/api/interpreter")
+# cityBoundaryResultMilan <- osmdata::opq_osm_id(id = dt_Metropolis[1,4], type = 'relation', open_url = FALSE) %>%
+#     osmdata::opq_string() %>%
+#     osmdata::osmdata_sf()
+# names(cityBoundaryResultMilan$osm_multipolygons$geometry[[1]][[1]]) <- NULL
+# cityBoundaryResultBuenoAires <- osmdata::opq_osm_id(id = dt_Metropolis[1,4], type = 'relation', open_url = FALSE) %>%
+#     osmdata::opq_string() %>%
+#     osmdata::osmdata_sf()
+# names(cityBoundaryResultBuenoAires$osm_multipolygons$geometry[[1]][[1]]) <- NULL
+# cityBoundaryResultSeville <- osmdata::opq_osm_id(id = dt_Metropolis[1,4], type = 'relation', open_url = FALSE) %>%
+#     osmdata::opq_string() %>%
+#     osmdata::osmdata_sf()
+# names(cityBoundaryResultSeville$osm_multipolygons$geometry[[1]][[1]]) <- NULL
+# cityBoundaryResultGuadajara <- osmdata::opq_osm_id(id = dt_Metropolis[1,4], type = 'relation', open_url = FALSE) %>%
+#     osmdata::opq_string() %>%
+#     osmdata::osmdata_sf()
+# names(cityBoundaryResultGuadajara$osm_multipolygons$geometry[[1]][[1]]) <- NULL
+# cityBoundaryResultMaribor <- osmdata::opq_osm_id(id = dt_Metropolis[1,4], type = 'relation', open_url = FALSE) %>%
+#     osmdata::opq_string() %>%
+#     osmdata::osmdata_sf()
+# names(cityBoundaryResultMaribor$osm_multipolygons$geometry[[1]][[1]]) <- NULL
+# cityBoundaryResultMexicoCity <- osmdata::opq_osm_id(id = dt_Metropolis[1,4], type = 'relation', open_url = FALSE) %>%
+#     osmdata::opq_string() %>%
+#     osmdata::osmdata_sf()
+# names(cityBoundaryResultMexicoCity$osm_multipolygons$geometry[[1]][[1]]) <- NULL
+metropolis  <- sf::st_read(
+  "cityGeojson/metropolis.shp")
+
+
+
+
 
--- server.R
+++ server.R
@@ -1,6 +1,26 @@
 function(input, output, session) {
 
   ## Interactive Map ##########################################
+  # Create the map
+  observeEvent(input$cities, {
+    if (input$cities == 'All metropolis') {
+      shinyjs::disable('semPackage')
+      updateSelectizeInput(
+        session,
+        'semPackage',
+        choices = dt_Metropolis[1,]['cityLabel'],
+        server = TRUE
+      )
+    } else if (input$cities != 'All metropolis') {
+      shinyjs::enable('semPackage')
+      updateSelectizeInput(
+        session,
+        'semPackage',
+        choices = dt_Metropolis[1,]['cityLabel'],
+        server = TRUE
+      )
+    }
+  })
   
   output$map <- leaflet::renderLeaflet({
     # Create a Progress object
@@ -9,12 +29,33 @@ function(input, output, session) {
     on.exit(progress$close())
     progress$set(message = "Please wait", value = 0)
     
-    map <- leaflet::leaflet() %>% 
-      leaflet::addTiles() %>% 
+    # add icons to the map
+    tellmeIcons <- leaflet::makeIcon(
+      iconUrl = 'www/icons/logo-tellme-1.jpg',
+      iconWidth = 18, iconHeight = 18
+    )
+    
+    map <- leaflet::leaflet(citiesGPS) %>%
+      leaflet::addTiles() %>%
+      leaflet::setView(0, 0, 2) %>%
+      leaflet::addMarkers(
+        popup = paste0(
+          "City: <b>", citiesGPS$cityLabel, "</b><br/>",
+          "<a href = '", citiesGPS$city, "'>Wikidata link</a>"
+        ),
+        icon = tellmeIcons,
+        group = 'Metropolis'
+      ) %>% 
       leaflet::addLayersControl(position = 'bottomright',
-                                #overlayGroups = c("Cities"),
+                                overlayGroups = c('Metropolis'),
                                 options = leaflet::layersControlOptions(collapsed = FALSE)
-      )
+      ) %>% 
+      leaflet::addPolygons(
+        data = metropolis, 
+        group = 'Metropolis',
+        weight = 2,
+        col = 'red', 
+        fillColor = 'red')
     
     # fitBound of phtos selected
     # observeEvent(input$cities, {
