7d13d2ddcd6dea8ef9424243dce411a305e6704b
Paolo Tagliolato <paolo.tagliolato@gmail.com>
2020-07-22 01:52:30 +0200

    Load initial metropolis polygons from files instead of computing them at startup. Add these files to project

--- global.R
+++ global.R
@@ -1,40 +1,6 @@
-library(dplyr)
-
-###
-# metropolis
-###
-### Cities points
-# probably this website could be a good started point https://www.metropolis.org/members-list
-# the list of metropolis is taken from wikidata
-# WikiData query for obtain the list of the cities with millions of inhabitants (https://www.wikidata.org/wiki/Q1637706)
-tableMetropolisWikiData <- WikidataQueryServiceR::query_wikidata(
-  'SELECT DISTINCT ?city ?cityLabel ?gps ?OSM_relation_ID ?population WHERE {
-  #?city wdt:P31 wd:Q1637706;
-  ?city wdt:P31 wd:Q515;
-    wdt:P625 ?gps;
-    wdt:P402 ?OSM_relation_ID;
-    wdt:P1082 ?population.
-  SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
-}'
-)
-
-# TODO add views parameters, geometry
-dt_Metropolis <- tableMetropolisWikiData %>% filter(cityLabel %in% c('Buenos Aires', 'Guadalajara', 'Seville', 'Milan'))
-dt_Metropolis <- dt_Metropolis %>% 
-  add_row(city = '', cityLabel = 'All metropolis', gps = 'Point(0 0)', OSM_relation_ID = 0, .before = 1) %>% 
-  add_row(city = 'https://www.wikidata.org/wiki/Q1010', cityLabel = 'Maribor', gps = 'Point(15.64667 46.55472)', OSM_relation_ID = 8154489) %>%
-  add_row(city = 'https://www.wikidata.org/wiki/Q1489', cityLabel = 'Mexico City', gps = 'Point(-99.12766 19.42847)', OSM_relation_ID = 1376330)
-citiesGPS <- sf::st_as_sf(dt_Metropolis[c(2:7),], wkt = 'gps')
-sf::st_crs(citiesGPS) = 4326
-
-### City multipolygons
-metropolisPolygons  <- sf::st_read(
-  "cityGeojson/metropolis.gpkg"
-)
-metropolisPolygons$tellmeCityLabel <- c('Buenos Aires', 'Guadalajara', 'Maribor', 'Mexico City', 'Milan', 'Seville')
-
-#sf::st_write(metropolis,"cityGeojson/metropolis.gpkg")
-
+dt_Metropolis<-readRDS("cityGeojson/dt_Metropolis.rds")
+citiesGPS<-sf::st_read("cityGeojson/citiesGPS.gpkg")
+metropolisPolygons  <- sf::st_read("cityGeojson/metropolis2.gpkg")
 
 
 
